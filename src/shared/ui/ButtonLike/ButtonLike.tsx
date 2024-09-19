5fcd7a88047f4eca8b927b8ad1b82d8b4e16aa42
Alex Larin <larinalexd@yandex.ru>
2024-09-19 15:55:00 +0400

    Fix bug. Like button uncorrect for Safari Browser. Optimization svg like and style for it

--- src/shared/ui/ButtonLike/ButtonLike.tsx
+++ src/shared/ui/ButtonLike/ButtonLike.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { ButtonHTMLAttributes, PropsWithChildren } from 'react'
+import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 import styles from './ButtonLike.module.scss';
 
 interface IButtonLikeProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -9,10 +9,9 @@ interface IButtonLikeProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 const ButtonLike = ({ onClick, isActive, className }: PropsWithChildren<IButtonLikeProps>) => {
     return (
         <button onClick={onClick} className={clsx(styles.likeButton, isActive && styles.isActive, className && className)}>
-            <svg width="16" height="16" viewBox="0 0 16 16" className={styles.icon} xmlns="http://www.w3.org/2000/svg">
-                <path d="M4.85 1C2.72375 1 1 2.72173 1 4.84548C1 8.69096 5.55 12.1869 8 13C10.45 12.1869 15 8.69096 15 4.84548C15 2.72173 13.2762 1 11.15 1C9.848 1 8.6965 1.64569 8 2.63398C7.64499 2.1289 7.17336 1.71669 6.62504 1.43226C6.07672 1.14784 5.46785 0.999565 4.85 1Z" stroke="var(--color-stroke)" strokeLinecap="round" strokeLinejoin="round" />
+            <svg width="14" height="12" viewBox="-1 -1 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.85 9.6021e-07C1.72375 9.6021e-07 0 1.72173 0 3.84548C0 7.69096 4.55 11.1869 7 12C9.45 11.1869 14 7.69096 14 3.84548C14 1.72173 12.2762 9.6021e-07 10.15 9.6021e-07C8.848 9.6021e-07 7.6965 0.645692 7 1.63398C6.64499 1.1289 6.17336 0.71669 5.62504 0.432263C5.07672 0.147837 4.46785 -0.000435366 3.85 9.6021e-07Z" />
             </svg>
-
         </button>
     )
 }
